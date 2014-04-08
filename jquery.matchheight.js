94b97f420bbae6cb279bc1ccdca3bac6861db9dc
Ben DeMeyer <ben@bendemeyer.com>
2014-04-08 11:16:11 -0400

    Built in support for Media Query testing

--- jquery.matchheight.js
+++ jquery.matchheight.js
@@ -1,20 +1,16 @@
 (function ($) {
-	$.fn.matchHeight = function (arg) {
+	$.fn.matchHeight = function (mq, bool) {
 		$.fn.matchHeight.count = $.fn.matchHeight ? $.fn.matchHeight + 1 : 1;
+		var curr = $.fn.matchHeight.count;
 		
 		var _this = this;
 		
-		function match(hander) {
+		function match() {
 		
-			var run = true;
+			var query = testMediaQuery(mq);
+			var support = query || supportsMediaQueries();
 			
-			if (typeof(handler) == 'function') {
-				run = handler.call(_this);
-			} else if (handler !== undefined) {
-				run = handler;
-			}
-			
-			if (run) {
+			if (query || (!support && bool)) {
 				var maxHeight = 0;
 				
 				_this.each(function () {
@@ -22,9 +18,34 @@
 				});
 				
 				_this.css('height', maxHeight);
+				
 			} else {
+			
 				_this.css('height', '');
+				
 			}
+		};
+		
+		function testMediaQuery(mq) {
+
+			var matchMedia = window.matchMedia || window.msMatchMedia;
+			if (matchMedia) {
+				return matchMedia(mq).matches;
+			}
+
+			var div = document.createElement('div');
+			div.id = 'styleTester';
+			div.innerHTML = '<style> @media ' + mq + ' { #styleTester { position: absolute; } }</style>';
+			document.getElementsByTagName('body')[0].appendChild(div);
+			var bool = (window.getComputedStyle ? getComputedStyle(div, null) : div.currentStyle)['position'] == 'absolute';
+			div.parentNode.removeChild(div);
+
+			return bool;
+
+		};
+
+		function supportsMediaQueries() {
+			testMediaQuery('only all');
 		};
 		
 		$(window).on('resize.matchheight:' + curr, function () {
