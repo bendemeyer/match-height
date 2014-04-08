e90e2b4a0fb15ad6afe4be297f14656c8e60df69
Ben DeMeyer <ben@bendemeyer.com>
2014-04-08 00:08:31 -0400

    Fixing consistency issues with tabs and spaces

--- jquery.matchheight.js
+++ jquery.matchheight.js
@@ -1,12 +1,12 @@
 (function ($) {
-    $.fn.matchHeight = function (arg) {
-        $.fn.matchHeight.count = $.fn.matchHeight ? $.fn.matchHeight + 1 : 1;
+	$.fn.matchHeight = function (arg) {
+		$.fn.matchHeight.count = $.fn.matchHeight ? $.fn.matchHeight + 1 : 1;
 		
 		var _this = this;
 		
-        function match(hander) {
+		function match(hander) {
 		
-            var run = true;
+			var run = true;
 			
 			if (typeof(handler) == 'function') {
 				run = handler.call(_this);
@@ -14,29 +14,29 @@
 				run = handler;
 			}
 			
-            if (run) {
-                var maxHeight = 0;
+			if (run) {
+				var maxHeight = 0;
 				
-                _this.each(function () {
-                    maxHeight = $(this).css('height', '').height() > maxHeight ? $(this).height() : maxHeight;
-                });
+				_this.each(function () {
+					maxHeight = $(this).css('height', '').height() > maxHeight ? $(this).height() : maxHeight;
+				});
 				
-                _this.css('height', maxHeight);
-            } else {
-                _this.css('height', '');
-            }
-        };
+				_this.css('height', maxHeight);
+			} else {
+				_this.css('height', '');
+			}
+		};
 		
-        $(window).on('resize.matchheight:' + curr, function () {
-            match(arg);
-        });
+		$(window).on('resize.matchheight:' + curr, function () {
+			match(arg);
+		});
 		
-        _this.destroy = function () {
-            $(window).off('resize.matchheight:' + curr);
-        };
+		_this.destroy = function () {
+			$(window).off('resize.matchheight:' + curr);
+		};
 		
-        match(arg);
+		match(arg);
 		
-        return this;
-    };
+		return this;
+	};
 })(jQuery);
